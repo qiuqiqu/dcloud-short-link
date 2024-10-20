c890c0a6cb1ade560a4a11fd896048b23510aace
qiuqiqu <1961237983@qq.com>
2024-10-20 21:04:16 +0800

    redis 缓存图形验证码

--- dcloud-account/src/main/java/net/xdclass/controller/NotifyController.java
+++ dcloud-account/src/main/java/net/xdclass/controller/NotifyController.java
@@ -3,8 +3,10 @@ package net.xdclass.controller;
 import com.google.code.kaptcha.Producer;
 import lombok.extern.slf4j.Slf4j;
 import net.xdclass.service.NotifyService;
+import net.xdclass.util.CommonUtil;
 import net.xdclass.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
-
+ *
  **/
 
 @RestController
@@ -31,46 +34,57 @@ public class NotifyController {
     @Autowired
     private NotifyService notifyService;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
+
+    private static final long CAPTCHA_CODE_EXPIRED = 1000 * 10 *  60;
     /**
      * 生成验证码
+     *
      * @param request
      * @param response
      */
     @GetMapping("captcha")
-    public void getCaptcha(HttpServletRequest request, HttpServletResponse response){
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
 
         String captchaText = captchaProducer.createText();
-        log.info("验证码内容:{}",captchaText);
+        log.info("验证码内容:{}", captchaText);
 
-        //存储redis,配置过期时间 TODO
+        //存储redis,配置过期时间
+        redisTemplate.opsForValue().set(getCaptchaKey(request), captchaText,CAPTCHA_CODE_EXPIRED, TimeUnit.MILLISECONDS);
 
         BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
 
         try {
             ServletOutputStream outputStream = response.getOutputStream();
-            ImageIO.write(bufferedImage,"jpg",outputStream);
+            ImageIO.write(bufferedImage, "jpg", outputStream);
             outputStream.flush();
             outputStream.close();
         } catch (IOException e) {
-            log.error("获取流出错:{}",e.getMessage());
+            log.error("获取流出错:{}", e.getMessage());
         }
 
     }
 
     /**
      * 测试发送验证码接口-主要是用于对比优化前后区别
+     *
      * @return
      */
     @RequestMapping("send_code")
-    public JsonData sendCode(){
+    public JsonData sendCode() {
 
         return JsonData.buildSuccess();
     }
 
-
-
-
+    private String getCaptchaKey(HttpServletRequest request) {
+        String IP = CommonUtil.getIpAddr(request);
+        String userAgent = request.getHeader("User-Agent");
+        String key="account-service:captcha:"+CommonUtil.MD5(IP+userAgent);
+        log.info("redis 图形验证码 key:{}",key);
+        return key;
+    }
 
 
 }
