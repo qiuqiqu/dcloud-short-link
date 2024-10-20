eadd10093d74d235fffc572e591a408aab4c83e1
qiuqiqu <1961237983@qq.com>
2024-10-20 16:52:56 +0800

    http连接池

--- dcloud-common/src/main/java/net/xdclass/config/RestTemplateConfig.java
+++ dcloud-common/src/main/java/net/xdclass/config/RestTemplateConfig.java
@@ -1,5 +1,15 @@
 package net.xdclass.config;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -21,11 +31,57 @@ public class RestTemplateConfig {
 
 
     @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
+    public ClientHttpRequestFactory httpRequestFactory(){
         SimpleClientHttpRequestFactory factory  = new SimpleClientHttpRequestFactory();
         factory.setReadTimeout(10000);
         factory.setConnectTimeout(10000);
         return factory;
+    }
+
+    @Bean
+    public HttpClient httpClient(){
+
+
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+
+
+        //设置连接池最大是500个连接
+        connectionManager.setMaxTotal(500);
+        //MaxPerRoute是对maxtotal的细分，每个主机的并发最大是300，route是指域名
+        connectionManager.setDefaultMaxPerRoute(300);
+        /**
+         * 只请求 xdclass.net,最大并发300
+         *
+         * 请求 xdclass.net,最大并发300
+         * 请求 open1024.com,最大并发200
+         *
+         * //MaxtTotal=400 DefaultMaxPerRoute=200
+         * //只连接到http://xdclass.net时，到这个主机的并发最多只有200；而不是400；
+         * //而连接到http://xdclass.net 和 http://open1024.com时，到每个主机的并发最多只有200；
+         * // 即加起来是400（但不能超过400）；所以起作用的设置是DefaultMaxPerRoute。
+         *
+         */
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                //返回数据的超时时间
+                .setSocketTimeout(20000)
+                //连接上服务器的超时时间
+                .setConnectTimeout(10000)
+                //从连接池中获取连接的超时时间
+                .setConnectionRequestTimeout(1000)
+                .build();
+
+
+        CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(connectionManager)
+                .build();
+
+        return closeableHttpClient;
     }
 
 
