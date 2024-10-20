95c4e920204654e1804c65463230a0bd37ba7a17
qiuqiqu <1961237983@qq.com>
2024-10-20 18:45:48 +0800

    图形验证码配置和接口开发

--- dcloud-account/src/main/java/net/xdclass/controller/NotifyController.java
+++ dcloud-account/src/main/java/net/xdclass/controller/NotifyController.java
@@ -1,11 +1,20 @@
 package net.xdclass.controller;
 
+import com.google.code.kaptcha.Producer;
 import lombok.extern.slf4j.Slf4j;
 import net.xdclass.service.NotifyService;
 import net.xdclass.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
 
@@ -16,9 +25,38 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class NotifyController {
 
+    @Autowired
+    private Producer captchaProducer;
 
     @Autowired
     private NotifyService notifyService;
+
+
+    /**
+     * 生成验证码
+     * @param request
+     * @param response
+     */
+    @GetMapping("captcha")
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response){
+
+        String captchaText = captchaProducer.createText();
+        log.info("验证码内容:{}",captchaText);
+
+        //存储redis,配置过期时间 TODO
+
+        BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
+
+        try {
+            ServletOutputStream outputStream = response.getOutputStream();
+            ImageIO.write(bufferedImage,"jpg",outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            log.error("获取流出错:{}",e.getMessage());
+        }
+
+    }
 
     /**
      * 测试发送验证码接口-主要是用于对比优化前后区别
