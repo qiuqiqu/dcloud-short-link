b238cd125b89ade717da41b4f00a1b872c510050
qiuqiqu <1961237983@qq.com>
2024-10-21 14:13:10 +0800

    图片文件上传

--- dcloud-account/src/main/java/net/xdclass/controller/AccountController.java
+++ dcloud-account/src/main/java/net/xdclass/controller/AccountController.java
@@ -1,9 +1,16 @@
 package net.xdclass.controller;
 
 
+import net.xdclass.enums.BizCodeEnum;
+import net.xdclass.service.FileService;
+import net.xdclass.util.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -12,7 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/account")
 public class AccountController {
 
-
+    @Autowired
+    private FileService fileService;
+    /**
+     * 上传用户头像
+     *
+     * 默认最大是1M,超过则报错
+     *
+     * @param file 文件
+     * @return
+     */
+    @PostMapping( "/upload")
+    public JsonData uploadUserImg(@RequestPart("file") MultipartFile file){
+        String result = fileService.uploadUserImg(file);
+        return result!=null? JsonData.buildSuccess(result):JsonData.buildResult(BizCodeEnum.FILE_UPLOAD_USER_IMG_FAIL);
+    }
 
 }
 
