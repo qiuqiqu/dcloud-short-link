73f9756d1bfaf048bd3589e043f3c6e2776137a6
qiuqiqu <1961237983@qq.com>
2024-10-21 13:48:32 +0800

    短信验证码放盗刷

--- dcloud-account/src/main/java/net/xdclass/service/impl/NotifyServiceImpl.java
+++ dcloud-account/src/main/java/net/xdclass/service/impl/NotifyServiceImpl.java
@@ -3,14 +3,20 @@ package net.xdclass.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import net.xdclass.component.SmsComponent;
 import net.xdclass.config.SmsConfig;
+import net.xdclass.constant.RedisKey;
+import net.xdclass.enums.BizCodeEnum;
 import net.xdclass.enums.SendCodeEnum;
 import net.xdclass.service.NotifyService;
 import net.xdclass.util.CheckUtil;
 import net.xdclass.util.CommonUtil;
 import net.xdclass.util.JsonData;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Version 1.0
@@ -20,6 +26,10 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class NotifyServiceImpl implements NotifyService {
 
+    /**
+     * 10分钟有效
+     */
+    private static final int CODE_EXPIRED = 60*1000*10;
 
     @Autowired
     private SmsComponent smsComponent;
@@ -30,20 +40,40 @@ public class NotifyServiceImpl implements NotifyService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
 
     @Override
     public JsonData sendCode(SendCodeEnum sendCodeEnum, String to) {
 
 
         String code = CommonUtil.getRandomCode(6);
+        // key= code:类型:邮箱或手机号
+        String smsKey = String.format(RedisKey.CHECK_CODE_KEY, sendCodeEnum, to);
+        String smsCacheValue = redisTemplate.opsForValue().get(smsKey);
+        //如果不为空，再判断是否是60秒内重复发送 0122_232131321314132
+        if (StringUtils.isNotBlank(smsCacheValue)){
+            long ttl = Long.parseLong(smsCacheValue.split("_")[1]);
+            //当前时间戳-验证码发送的时间戳，如果小于60秒，则不给重复发送
+            long leftTime = CommonUtil.getCurrentTimestamp()-ttl;
+            if( leftTime < (1000*60)){
+                log.info("重复发送短信验证码，时间间隔:{}秒",leftTime);
+                return JsonData.buildResult(BizCodeEnum.CODE_LIMITED);
+            }
+        }
 
-        if(CheckUtil.isEmail(to)){
+        //生成拼接好验证码
+        String value = code+"_"+CommonUtil.getCurrentTimestamp();
+        redisTemplate.opsForValue().set(smsKey,value,CODE_EXPIRED, TimeUnit.MILLISECONDS);
+
+        if (CheckUtil.isEmail(to)) {
             //发送邮箱验证码  TODO
 
-        }else if(CheckUtil.isPhone(to)){
+        } else if (CheckUtil.isPhone(to)) {
 
             //发送手机验证码
-            smsComponent.send(to,smsConfig.getTemplateId(),code);
+            smsComponent.send(to, smsConfig.getTemplateId(), code);
 
         }
 
---
+++ dcloud-common/src/main/java/net/xdclass/constant/RedisKey.java
@@ -0,0 +1,15 @@
+package net.xdclass.constant;
+
+/**
+ *  redis 短信 key 常量
+**/
+
+public class RedisKey {
+
+/**
+* 验证码缓存key，第一个是类型,第二个是唯一标识比如手机号或者邮箱
+*/
+public static final String CHECK_CODE_KEY = "code:%s:%s";
+
+
+}
