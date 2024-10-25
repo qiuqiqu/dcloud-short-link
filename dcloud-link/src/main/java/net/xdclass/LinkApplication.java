d19eb983d49346b57d682d889e84de278b7f2670
qiuqiqu <1961237983@qq.com>
2024-10-25 22:00:29 +0800

    组件ShortLinkComponent 测试

---
+++ dcloud-link/src/main/java/net/xdclass/LinkApplication.java
@@ -0,0 +1,22 @@
+package net.xdclass;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@MapperScan("net.xdclass.mapper")
+@EnableTransactionManagement
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkApplication.class, args);
+    }
+
+}
